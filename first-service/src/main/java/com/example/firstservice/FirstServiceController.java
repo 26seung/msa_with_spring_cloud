b5af35e2145fa63318ebf9e37e32a46922e689e4
Euseung <yousi7@naver.com>
2022-10-10 23:44:11 +0900

    first-service

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,2 +1,20 @@
-package com.example.firstservice;public class FirstServiceController {
+package com.example.firstservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+//  gateway 등록한 uri 와 predicates 의 설정으로
+//  http:localhost:8000/first(second)-service/**
+//  형식으로 매핑이 진행 됨
+
+@RestController
+@RequestMapping("/first-service")
+public class FirstServiceController {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome to the First service";
+    }
 }
