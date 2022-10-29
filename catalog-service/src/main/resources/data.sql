973efe4d4410f0593a66623d4982ebe292cca484
Euseung <yousi7@naver.com>
2022-10-30 03:15:18 +0900

    catalog

---
+++ catalog-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO catalog (product_id, product_name, stock, unit_price) VALUES ('CATALOG-001', 'Berlin', 100, 1500);
+INSERT INTO catalog (product_id, product_name, stock, unit_price) VALUES ('CATALOG-002', 'Tokyo', 110, 1000);
+INSERT INTO catalog (product_id, product_name, stock, unit_price) VALUES ('CATALOG-003', 'Korea', 200, 2000);
