200d89b0b5c4abc839fec8a5602ba428cef41a7a
Euseung <yousi7@naver.com>
2022-10-31 00:48:46 +0900

    catalog

--- catalog-service/src/main/java/com/example/catalogservice/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/example/catalogservice/CatalogServiceApplication.java
@@ -2,8 +2,14 @@ package com.example.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+// 사용하지 않아도 인스턴스가 등록됩니다.
+// DiscoveryClient 인터페이스를 구현한 클래스를 검색해서 Service Registry에 등록하게 되는데,
+// 기본적으로 Spring Boot 서버를 자동으로 등록하도록 설정되어 있습니다.
+// 그러나, 이 설정은 autoRegister=false 에 의해서 변경될수도 있기 때문에, 작업해 주시는게 좋다고 생각됩
 public class CatalogServiceApplication {
 
 	public static void main(String[] args) {
---
+++ catalog-service/src/main/java/com/example/catalogservice/controller/CatalogController.java
@@ -0,0 +1,42 @@
+package com.example.catalogservice.controller;
+
+import com.example.catalogservice.jpa.CatalogEntity;
+import com.example.catalogservice.service.CatalogService;
+import com.example.catalogservice.vo.ResponseCatalog;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog-service")
+@RequiredArgsConstructor
+public class CatalogController {
+
+    private final Environment environment;
+    private final CatalogService catalogService;
+
+
+    @GetMapping("/health_check")
+    public String status(){
+        return String.format("It's Working in User Service on PORT %s",environment.getProperty("local.server.port"));
+    }
+
+    @GetMapping("/catalogs")
+    public ResponseEntity<List<ResponseCatalog>> getUser(){
+        Iterable<CatalogEntity> catalogList = catalogService.getAllCatalogs();
+
+        List<ResponseCatalog> result = new ArrayList<>();
+        catalogList.forEach(v -> {
+            result.add(new ModelMapper().map(v, ResponseCatalog.class));
+        });
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+}
